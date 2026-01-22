2b84fbcde11d8b9e54efc72b2349574f6e979221
MuhammedJaseel <jaseelmanamulli@gmail.com>
2026-01-23 01:24:26 +0400

    OP

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,7 +9,7 @@ export class AppService {
   constructor(@InjectModel(AccesLog.name) private logModel: Model<AccesLog>) {}
 
   getHello(): string {
-    return 'ANOWEB! (0.0.3)';
+    return 'ANOWEB! (0.0.4)';
   }
 
   async _create(domain: string, ip: string) {
