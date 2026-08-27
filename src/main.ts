68ae3f12ae352e6ad2f485e3e593583a02458250
MAG-Jaseel <jaseel@mediaagencygroup.ae>
2026-08-27 08:27:49 +0530

    OP

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,12 @@ const PORT = process.env.PORT || 4680;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(PORT);
   console.log(`Running on: http://localhost:${PORT}`);
 }
